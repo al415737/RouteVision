7f6ec26286250855c4584e0e31640efb2109d211
ABEL <al415746@uji.es>
2024-12-20 20:33:28 +0100

    test(it01h07): test integracion realizados + pasan los tests

--- src/app/tests/integración/placeIntegrationService.spec.ts
+++ src/app/tests/integración/placeIntegrationService.spec.ts
@@ -1,66 +1,125 @@
 import { TestBed } from '@angular/core/testing';
-import { getFirestore, provideFirestore } from '@angular/fire/firestore';
-import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
-import { getAuth, provideAuth } from '@angular/fire/auth';
-
-import { PLACE_REPOSITORY_TOKEN, PlaceRepository } from '../../repositorios/interfaces/place-repository';
-import { provideHttpClient } from '@angular/common/http';
-import { firebaseConfig } from '../../app.config';
 import { PlaceService } from '../../servicios/place.service';
-import { PlaceFirebaseService } from '../../repositorios/firebase/place-firebase.service';
-import { FirestoreService } from '../../repositorios/firebase/firestore.service';
-import { Observable } from 'rxjs';
 import { Place } from '../../modelos/place';
+import { PLACE_REPOSITORY_TOKEN, PlaceRepository } from '../../repositorios/interfaces/place-repository';
+import { GeocodingService } from '../../APIs/Geocoding/geocoding.service';
+import { provideHttpClient } from '@angular/common/http';
+import { PlaceFirebaseService } from '../../repositorios/firebase/place-firebase.service';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { provideFirebaseApp } from '@angular/fire/app';
+import { initializeApp } from 'firebase/app';
+import { firebaseConfig } from '../../app.config';
+import { provideAuth } from '@angular/fire/auth';
+import { getAuth } from 'firebase/auth';
+import { InvalidCoordenatesException } from '../../excepciones/invalid-coordenates-exception';
+import { InvalidPlaceException } from '../../excepciones/invalid-place-exception';
+import { ServerNotOperativeException } from '../../excepciones/server-not-operative-exception';
 
 describe('PlaceIntegrationService', () => {
+
   let servicePlace: PlaceService;
-  let placeRepository: PlaceRepository;
+  let geocodingRepositorio: GeocodingService;
+  let placeRepositorio: PlaceRepository;
 
   beforeEach(() => {
-    placeRepository = new PlaceFirebaseService();
-    spyOn(placeRepository, "createPlaceC");
-    spyOn(placeRepository, "createPlaceT");
-    spyOn(placeRepository, "deletePlace");
     TestBed.configureTestingModule({
       providers: [
-        provideHttpClient(),
+        provideHttpClient(), // Configuración moderna para HttpClient
         provideFirebaseApp(() => initializeApp(firebaseConfig)),
         provideFirestore(() => getFirestore()),
         provideAuth(() => getAuth()),
         PlaceService,
-        { provide: PLACE_REPOSITORY_TOKEN, use
-            ç: PlaceFirebaseService },
-        FirestoreService,
+        { provide: PLACE_REPOSITORY_TOKEN, useClass: PlaceFirebaseService }
       ]
-    });
+    }).compileComponents();
+
+    placeRepositorio = TestBed.inject(PLACE_REPOSITORY_TOKEN);
 
     servicePlace = TestBed.inject(PlaceService);
+    geocodingRepositorio = TestBed.inject(GeocodingService); 
   });
 
+  //HISTORIA 5
+  fdescribe('PlaceService', () => {  //HISTORIA 5
+    it('PRUEBA INTEGRACIÓN --> HU5E01. Registrar nuevo lugar de interés (Caso Válido):', async () => {
+      const mockPlace: Place = new Place('001', "Castellón de la Plana", [39.98, -0.049]);
 
-  it('HU7E01. Consulta de lista de lugares dados de alta (Escenario válido):', async () => {
-    spyOn(placeRepository, 'getPlaces').and.returnValue(
-        Promise.resolve([])
-      );
-    const lugares = await servicePlace.getPlaces();
-    expect(lugares.length).toBe(2);
-    lugares.forEach((lugar: Place) => {
-                expect(lugar).toBeInstanceOf(Place);
-            });
+      spyOn(placeRepositorio, 'createPlaceC').and.resolveTo(mockPlace);
+      
+      const result = await servicePlace.createPlaceC([39.98, -0.049]);
+      expect(placeRepositorio.createPlaceC).toHaveBeenCalledWith([39.98, -0.049]);
+      expect(result).toEqual(mockPlace);
+    });
+  })
+
+  fdescribe('PlaceService', () => {  
+    it('PRUEBA INTEGRACIÓN --> HU5E02. Registro de un lugar de interés incorrecto (Caso Inválido):', async () => {
+      const mockPlace: Place = new Place('001', "Castellón de la Plana", [39.98, -0.049]);
+
+      spyOn(placeRepositorio, 'createPlaceC').and.resolveTo(mockPlace);
+    
+      try{
+        servicePlace.createPlaceC([899.99, ]);
+        expect(placeRepositorio.createPlaceC).toHaveBeenCalledWith([899.99, ]);
+      } catch(error) {
+        expect(error).toBeInstanceOf(InvalidCoordenatesException);
+      }
+    });
+  })
+
+
+  //HISTORIA 6
+  fdescribe('PlaceService', () => {  //HISTORIA 5
+    it('PRUEBA INTEGRACIÓN --> HU6E01. Registro de lugar de interés con un topónimo correcto (Escenario Válido):', async () => {
+      const mockPlace: Place = new Place('002', "Bilbao", [43.26271, -2.92528]);
+
+      spyOn(placeRepositorio, 'createPlaceT').and.resolveTo(mockPlace);
+      
+      const result = await servicePlace.createPlaceT('Bilbao');
+      expect(placeRepositorio.createPlaceT).toHaveBeenCalledWith('Bilbao');
+      expect(result).toEqual(mockPlace);
+    });
   });
 
-  /*it('HU7E02. Consulta de lista de lugares dados de alta sin conexión a la BBDD (Escenario inválido):', async () => {
-    spyOn(serviceUser, 'loginUser').and.returnValue(Promise.resolve());
-    spyOn(servicePlace, 'createPlaceC').and.returnValue(Promise.resolve(new Place('000', 'Castelló de la Plana', [39.98, -0.049])));
-    spyOn(servicePlace, 'createPlaceT').and.returnValue(Promise.resolve(new Place('001', 'Bilbao', [43.26271, -2.92528])));
-    spyOn(serviceUser, 'logoutUser').and.returnValue(Promise.resolve());
-    spyOn(servicePlace, 'getPlaces').and.returnValue(Promise.reject(new ServerNotOperativeException()));
+  fdescribe('PlaceService', () => {  
+    it('PRUEBA INTEGRACIÓN --> HU6E01. Registro de lugar de interés con un topónimo correcto (Escenario Válido):', async () => {
+      const mockPlace: Place = new Place('002', "Bilbao", [43.26271, -2.92528]);
 
-    await serviceUser.loginUser("test@test.com", "test123");
-    const lugar1 = await servicePlace.createPlaceC([39.98, -0.049]);
-    const lugar2 = await servicePlace.createPlaceT("Bilbao");
-    await serviceUser.logoutUser();
+      spyOn(placeRepositorio, 'createPlaceT').and.resolveTo(mockPlace);
+    
+      try{
+        servicePlace.createPlaceT('Cassjdlftellfisonon');
+        expect(placeRepositorio.createPlaceT).toHaveBeenCalledWith('Cassjdlftellfisonon');
+      } catch(error) {
+        expect(error).toBeInstanceOf(InvalidPlaceException);
+      }
+    });
+  });
 
-    await expectAsync(servicePlace.getPlaces()).toBeRejectedWith(new ServerNotOperativeException());
-  });*/ 
-});
+  fdescribe('PlaceService', () => {  
+    it('PRUEBA INTEGRACIÓN --> HU7E01. Consulta de lista de lugares dados de alta (Escenario válido):', async () => {
+      const mockPlace: Place[] = [new Place('001', "Castellón de la Plana", [39.98, -0.049]), new Place('002', "Barcelona", [33.98, -0.049]),];
+
+      spyOn(placeRepositorio, 'getPlaces').and.resolveTo(mockPlace);
+      
+      const result = await servicePlace.getPlaces();
+      expect(placeRepositorio.getPlaces).toHaveBeenCalledWith();
+      expect(result).toEqual(mockPlace);
+    });
+  });
+
+  fdescribe('PlaceService', () => {  
+    it('PRUEBA INTEGRACIÓN --> HU7E02. Consulta de lista de lugares dados de alta sin conexión a la BBDD (Escenario inválido):', async () => {
+      const mockPlace: Place[] = [new Place('001', "Castellón de la Plana", [39.98, -0.049]), new Place('002', "Barcelona", [33.98, -0.049]),];
+
+      spyOn(placeRepositorio, 'getPlaces').and.resolveTo(mockPlace);
+    
+      try{
+        servicePlace.getPlaces();
+        expect(placeRepositorio.getPlaces).toHaveBeenCalledWith();
+      } catch(error) {
+        expect(error).toBeInstanceOf(ServerNotOperativeException);
+      }
+    });
+  });
+})
