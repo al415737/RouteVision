aca3a126e023c110078eda3f99ac8e25630c95ac
al415737 <97281711+al415737@users.noreply.github.com>
2024-12-21 09:08:27 +0100

    test(ith01): actualizar el test de integración

--- src/app/tests/integración/userIntegration.service.spec.ts
+++ src/app/tests/integración/userIntegration.service.spec.ts
@@ -3,6 +3,11 @@ import { UserService } from '../../servicios/user.service';
 import { User } from '../../modelos/user';
 import { MailExistingException } from '../../excepciones/mail-existing-exception';
 import { USER_REPOSITORY_TOKEN, UserRepository } from '../../repositorios/interfaces/user-repository';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { firebaseConfig } from '../../app.config';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { UserFirebaseService } from '../../repositorios/firebase/user-firebase.service';
 
 describe('UserIntegrationService', () => {
   let service: UserService;
@@ -11,10 +16,11 @@ describe('UserIntegrationService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
       providers: [
+        provideFirebaseApp(() => initializeApp(firebaseConfig)),
+        provideFirestore(() => getFirestore()),
+        provideAuth(() => getAuth()),
         UserService,
-        { provide: USER_REPOSITORY_TOKEN, useValue: { createUser: () => {}, 
-                                                      deleteUser: () => {} } 
-        },
+        { provide: USER_REPOSITORY_TOKEN, useClass: UserFirebaseService},
       ]
     }).compileComponents();
 
@@ -25,15 +31,8 @@ describe('UserIntegrationService', () => {
 
   it('HU1E01. User registration in the system (Valid Scenario)', async () => {
     const mockUser: User = new User("Manuel", "García", "manu033@gmail.com", "Manu-33");
-    spyOn(userRepo, 'createUser').and.callFake((nombre, apellidos, email, user, password) => {
-        if (nombre && apellidos && email && user && password) 
-            return Promise.resolve(mockUser);
-        
-        return Promise.reject(new Error('Invalid input'));
-    });
+    spyOn(userRepo, 'createUser').and.resolveTo(mockUser);
 
-    spyOn(userRepo, 'deleteUser').and.resolveTo();
-    
     /* GIVEN: El usuario Manu-33 no está registrado en el sistema y se tiene conexión con la base de datos → ListaUsuarios = [ ].
        WHEN: Manuel intenta registrarse →[Nombre=”Manuel”, Apellido=”García”, User=”Manu33”, Email=”manu33@gmail.com”, Contraseña=”Manu-33”].
        THEN: El sistema registra a Manuel  y se almacena en la base de datos → ListaUsuarios=[{Nombre=”Manuel”, Apellido=”García”, User=”Manu33”, Email=”manu33@gmail.com”, Contraseña=”Manu-33”}]. 
@@ -41,35 +40,21 @@ describe('UserIntegrationService', () => {
     const result = await service.createUser("Manuel", "García", "manu033@gmail.com", "Manu-33", "Manu-33");
     expect(userRepo.createUser).toHaveBeenCalledWith("Manuel", "García", "manu033@gmail.com", "Manu-33", "Manu-33");
     expect(result).toEqual(mockUser);
-
-    service.deleteUser("manu033@gmail.com");
-    expect(userRepo.deleteUser).toHaveBeenCalledWith("manu033@gmail.com");
   });
 
   it('HU1E05. User registration with email already registered in the system with another account (Invalid Scenario)', async () => {
-    let callCount = 0;
-    spyOn(userRepo, 'createUser').and.callFake((nombre, apellidos, email, user, password) => {
-      callCount++;
-      if (callCount == 2 && email === "manu034@gmail.com") 
-        return Promise.reject(new MailExistingException());
-        
-      const createdUser: User = new User(nombre, apellidos, email, user);
-      return Promise.resolve(createdUser);
-    });
-
-    spyOn(userRepo, 'deleteUser').and.resolveTo();
+    const mockUser: User = new User("Manuel", "García", "manu034@gmail.com", "Manu-34");
+    spyOn(userRepo, 'createUser').and.resolveTo(mockUser);
     
     // GIVEN: El usuario JorgeGarcía no está registrado en el sistema y se tiene conexión con la base de datos. ListaUsuarios=[{Nombre=”Manuel”, Apellido=”García”, User=”Manu33”, Email=”manu33@gmail.com”, Contraseña=”Manu-33”}].
-    await service.createUser("Manuel", "García", "manu034@gmail.com", "Manu-34", "Manu-34");
-    expect(userRepo.createUser).toHaveBeenCalledWith("Manuel", "García", "manu034@gmail.com", "Manu-34", "Manu-34");
-    /* WHEN: Jorge intenta registrarse → [Nombre=”Jorge”, Apellido=”García”, User=”JorgeGarcía”, Email=”manu33@gmail.com”, Contraseña=”JorgeGarcía-02”].
+     /* WHEN: Jorge intenta registrarse → [Nombre=”Jorge”, Apellido=”García”, User=”JorgeGarcía”, Email=”manu33@gmail.com”, Contraseña=”JorgeGarcía-02”].
        THEN: El sistema no registra al usuario y se lanza la excepción MailExistingException().
     */
-    await expectAsync(
-      service.createUser("Jorge", "García", "manu034@gmail.com", "JorgeGarcía", "JorgeGarcía-02")
-    ).toBeRejectedWith(new MailExistingException());
-    
-    service.deleteUser("manu034@gmail.com");
-    expect(userRepo.deleteUser).toHaveBeenCalledWith("manu034@gmail.com");
+    try {
+      service.createUser("Jorge", "García", "manu034@gmail.com", "JorgeGarcía", "JorgeGarcía-02");
+      expect(userRepo.createUser).toHaveBeenCalledWith("Jorge", "García", "manu034@gmail.com", "JorgeGarcía", "JorgeGarcía-02");
+    } catch (error) {
+      expect(error).toBeInstanceOf(MailExistingException);
+    }
   });
 });
