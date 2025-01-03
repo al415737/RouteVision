0d7758349c86973773ce97497d1cfd0f4045e94a
IreneMartinezSancho <al404979@uji.es>
2025-01-03 11:16:23 +0100

    Feat(it05): Mejora visual html home.

--- src/app/componentes/home/home.component.ts
+++ src/app/componentes/home/home.component.ts
@@ -16,23 +16,6 @@ export default class HomeComponent {
   private authState: AuthStateService = inject(AuthStateService);
   datos:any;
   ngOnInit(): void {
-    this._luzApi.getPrecios().subscribe({
-      next: (response) => {
-        this.datos = response.included; // Guardar los datos obtenidos
-        const pvpc = this.datos.find((item: any) => item.type === "PVPC");
-        const pvpcValues = pvpc.attributes.values.map((pvpcItem: { datetime: string; value: number }) => ({
-          datetime: pvpcItem.datetime,
-          pvpc_value: pvpcItem.value,
-        }));
-  
-        console.log(pvpcValues);
-      },
-      error: (error) => {
-        console.error('Error al obtener los datos:', error);
-      },
-      complete: () => {
-        console.log('Solicitud completada.');
-      },
-    });
+   
   }  
 }
