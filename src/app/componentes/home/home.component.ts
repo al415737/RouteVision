d734788b2dc32ea39b6db6df519e8e0963407fe5
IreneMartinezSancho <al404979@uji.es>
2025-01-03 18:32:01 +0100

    Feat(it05): Mejora visual html + ocultar pwd (5).

--- src/app/componentes/home/home.component.ts
+++ src/app/componentes/home/home.component.ts
@@ -1,7 +1,6 @@
 import { Component, inject } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { HeaderComponent } from "./header/header.component";
-import { PrecioLuzService } from '../../APIs/PrecioLuz/precioLuz.service';
 import { AuthStateService } from '../../utils/auth-state.service';
 
 @Component({
@@ -12,10 +11,5 @@ import { AuthStateService } from '../../utils/auth-state.service';
   styleUrl: './home.component.css'
 })
 export default class HomeComponent {
-  private _luzApi: PrecioLuzService = inject(PrecioLuzService);
-  private authState: AuthStateService = inject(AuthStateService);
-  datos:any;
-  ngOnInit(): void {
-   
-  }  
+  private _auth = inject(AuthStateService);  
 }
